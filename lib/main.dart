d67fb4104f0c48254eaa162d49a171bd2e4afb9b
KalebJeielDCunanan <kalebcunananpro27@gmail.com>
2026-03-17 10:54:04 +0800

    Removed Assets folder App is now statefull previousely it doesnt have any state

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,20 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-      MaterialApp(
-        title: "Hello, World!",
-        home: Scaffold(
-            backgroundColor: Colors.red,body: Center(child: Text("Hello World")),
-      )));
+  runApp(MyApp());
 }
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return
+      MaterialApp(
+          title: "Hello, World!",
+          home: Scaffold(
+            backgroundColor: Colors.red,body: Center(child: Text("Hello World")),
+          ));
+  }
+}
+
