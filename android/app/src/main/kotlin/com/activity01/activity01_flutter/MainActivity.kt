dae4f39628fbeec0876b343d9d7f43fc5de7423e
Kaleb Jeiel D. Cunanan <cunanankd@students.nu-clark.edu.ph>
2026-03-17 10:47:13 +0800

    Initial commit

---
+++ android/app/src/main/kotlin/com/activity01/activity01_flutter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.activity01.activity01_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity : FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+      MaterialApp(
+        title: "Hello, World!",
+        home: Scaffold(
+            backgroundColor: Colors.red,body: Center(child: Text("Hello World")),
+      )));
+}
